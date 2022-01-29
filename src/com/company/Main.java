3ca0dd85ad62ea683342318dcac56783e9757fb9
moazzamsadiq <moazzamsadiq94@gmail.com>
2022-01-29 11:53:24 -0500

    Feature Sun with Errors to be resolved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,10 @@ public class Main {
         milkyway.sun=1;
         milkyway.Moon=2;
         milkyway.Planets=3;
+
+        SUN Solana=new SUN();
+        Solana.Heat="6776767367kj";
+        Solana.radius="6473647634km";
+        Solana.temp=122131243c;
     }
 }
